55847744dfbf104652621560758262f537871466
haoyu wang <1363585348@qq.com>
2023-01-25 17:26:35 +0800

    Initial deployment completed

--- backend/src/main/java/com/bbs/backend/controller/battle/BotInfoController.java
+++ backend/src/main/java/com/bbs/backend/controller/battle/BotInfoController.java
@@ -2,16 +2,24 @@ package com.bbs.backend.controller.battle;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 @Controller标识的类，代表控制器类(控制层/表现层)。
 */
 @RestController //@controller和@ResponseBody 的结合,该类所有API返回的数据会以Json字符串的形式返回
 @RequestMapping("/battle/") //@RequestMapping(),用于映射请求
 public class BotInfoController {
-    @RequestMapping("getBotInfo/")
-    public String getBotInfo()
+    @RequestMapping("getbotinfo/")
+    public Map<String, String> getBotInfo()
     {
-        return "connect successfully";
+        Map<String, String> test_bot = new HashMap<>();
+        test_bot.put("name", "wolf");
+        test_bot.put("strength", "1997");
+
+        return test_bot;
     }
 
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div>Bot name:{{ bot_name }} </div>
+    <div>Bot strength: {{ bot_strength }}</div>
+
+  </div>
+  <router-view></router-view>
 </template>
 
+<script>
+import $ from 'jquery';
+import { ref } from 'vue'; //引入变量
+
+export default{
+  name: "App",
+  //函数主入口
+  setup: () => {
+    let bot_name = ref("");
+    let bot_strength = ref("");
+
+    //访问后端获取变量值
+    $.ajax({
+      url: "http://127.0.0.1:3000/battle/getbotinfo/", //该url为后端函数的返回值地址
+      type: "get",
+      success: resp =>{
+        bot_name.value = resp.name;
+        bot_strength.value = resp.strength;
+      } 
+    });
+    //返回给前端的值
+    return {
+      bot_name,
+      bot_strength
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body{
+    background-image: url("@/assets/background.jpg");
+    background-size: cover;
+  }
 </style>
