691925df622c217be76188aa5184bc6d8850de63
haoyu wang <1363585348@qq.com>
2023-01-30 15:53:22 +0800

    create user table

---
+++ backend/src/main/java/com/bbs/backend/controller/user/UserController.java
@@ -0,0 +1,45 @@
+package com.bbs.backend.controller.user;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.bbs.backend.mapper.UserMapper;
+import com.bbs.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserMapper userMapper;
+
+    //查询所有user
+    @GetMapping("user/all/")
+    public List<User> getAll() //查询user中的所有数据
+    {
+        return userMapper.selectList(null);
+    }
+
+    //根据id查询user
+    @GetMapping("user/{userId}/")
+    public User getUser(@PathVariable int userId)
+    {
+        /// return userMapper.selectById(userId);
+        ///////或者使用条件构造器QueryWrapper
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id", userId);//找到id为userId的id. eq：等于， gt：大于，ge：大于等于，同理 lt/le
+
+        return userMapper.selectOne(queryWrapper);
+    }
+
+    //删除
+    @GetMapping("user/delete/{userId}/")
+    public String deleteUser(@PathVariable int userId) {
+        userMapper.deleteById(userId);
+        return "Delete successfully!";
+    }
+}
